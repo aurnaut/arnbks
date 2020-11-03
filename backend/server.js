bfaedae6343bb2709fe30c153e4775bca0045551
Marius Arnautu <marn@Mariuss-Mac-mini.local>
2020-11-03 18:02:34 +0000

    try 8

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,11 @@ connection.once('open', () => {
 
 app.use(express.static('./public'));
 
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
 const booksRouter = require('./routes/books');
 const authorsRouter = require('./routes/authors');
 
