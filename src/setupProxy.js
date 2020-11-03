9751588a8ea6e759574dc557d24b39e9ed3a2e24
Marius Arnautu <marn@Mariuss-Mac-mini.local>
2020-11-03 16:52:20 +0000

    add single script call for ui and server and modify the uploads proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/uploads', 
+    app.use(createProxyMiddleware('/backend/uploads', 
         { target: 'http://localhost:5000/' }
     ));
 }
