a7b322f8aa66c175881ba854a1d246b95e9ab052
Marius Arnautu <marn@Mariuss-Mac-mini.local>
2020-11-04 20:36:00 +0000

    modify

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
-
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
