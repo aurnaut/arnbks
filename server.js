b4f005d8f336b9957c55ab79f5e309f6613fe7a3
Marius Arnautu <marn@Mariuss-Mac-mini.local>
2021-04-01 11:25:19 +0100

    use prod build instead of localhost

--- server.js
+++ server.js
@@ -27,14 +27,14 @@ const categoriesRouter = require('./routes/categories');
 app.use('/api/books', booksRouter);
 app.use('/api/categories', categoriesRouter);
 
-//Local usage
-app.use(express.static(path.join(__dirname, 'public')))
+// //Local usage
+// app.use(express.static(path.join(__dirname, 'public')))
 
 //Prod usage
-// app.use(express.static( 'client/build' ));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
-// });
+app.use(express.static( 'client/build' ));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port:${port}`);
