eabca439dd57669cb3bc3db7aa0b806db509071a
Marius Arnautu <marn@Mariuss-Mac-mini.local>
2021-04-01 11:55:58 +0100

    add use statements back

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/books', booksRouter);
 app.use('/api/categories', categoriesRouter);
 
 // //Local usage
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Prod usage
 app.use(express.static( 'client/build' ));
